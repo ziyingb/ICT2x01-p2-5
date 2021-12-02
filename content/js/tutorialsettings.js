bc03f2457b8c88643670b64beadbb4f89c1c9ec6
ziyingb <48317019+ziyingb@users.noreply.github.com>
2021-12-02 23:00:41 +0800

    Update tutorialsettings.js

--- content/js/tutorialsettings.js
+++ content/js/tutorialsettings.js
@@ -1,22 +1,10 @@
-// var triggerTabList = [].slice.call(document.querySelectorAll('#myTab button'))
-// triggerTabList.forEach(function (triggerEl) {
-//   var tabTrigger = new bootstrap.Tab(triggerEl)
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab button'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
 
-//   triggerEl.addEventListener('click', function (event) {
-//     event.preventDefault()
-//     tabTrigger.show()
-//   })
-// })
-//open catergory modal
-$('#CategoryModal').on('click',".viewCategoryModal",function(e){
-  var tabTrigger = e.currentTarget.dataset;
-  console.log(tabTrigger)
-  $('#tutorial').val(tabTrigger.tutorial);
-  $('#delay').val(tabTrigger.delay);
-  $('#dcmotor').val(tabTrigger.dcmotor);
-  $('#loops').val(tabTrigger.loops);
-  $('#ifelse').val(tabTrigger.ifelse);
-  $('#custom').val(tabTrigger.custom);
-  
-  $('#viewCategoryModal').modal('show');
-});
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+
